a6e2ea876c3578e225bbcc2bff72c76272b3b54f
Jakub Piotrowski <226413@student.pwr.edu.pl>
2018-04-12 12:34:44 +0200

    nokia meeting

--- database/express-sequelize-test/app.js
+++ database/express-sequelize-test/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const puppeteer = require('puppeteer');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var comments = require('./routes/comments');
@@ -15,7 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+const bp=bodyParser.json();
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -27,24 +27,58 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/comments', comments);
-app.use('/convert', convertsRouter);
+app.post('/convert', bp,(req, res) => {
+
+    /*const browser = await puppeteer.launch({
+        headless:false
+    });
+    const page = await browser.newPage();
+    await page.goto(req.body.url,{waitUntil : "networkidle2"});
+    await page.screenshot({path: 'public/'+req.body.name, fullPage:true});
+    await browser.close();*/
+    console.log(req);
+puppeteer.launch({
+    headless: false
+}).then((browser) => {
+    //console.log(browser);
+    browser.newPage()
+        .then(page => page.goto(req.body.url, {waitUntil: "networkidle2"})
+            .then(resp => page.screenshot({path: 'public/' + req.body.name, fullPage: true})
+                .then(buffer => {
+                    browser.close();
+                    //convert.use(express.static('public'));
+                    res.header("Access-Control-Allow-Origin", "*");
+                    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                    //res.send('http://localhost:3000/' + req.body.name)
+                    res.status(200).send("xdxd");
+                })))
+}, (err) => {
+    console.error(err);
+    res.status(500);
+});
+
+
+
+
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+var err = new Error('Not Found');
+err.status = 404;
+next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// render the error page
+res.status(err.status || 500);
+res.render('error');
 });
 
 module.exports = app;
--- database/express-sequelize-test/routes/convert.js
+++ database/express-sequelize-test/routes/convert.js
@@ -1,29 +1,45 @@
 const express = require('express');
-const router = express.Router();
+//const router = express.Router();
 const puppeteer = require('puppeteer');
-var convert = express();
+//var convert = express();
+module.exports = function (app,bodyparser){
+    app.post('/', bodyparser,(req, res) => {
 
 
-/* POST a url and name of file to get a png convert of it*/
-router.post('/', async function (req, res){
-    try {
-        const browser = await puppeteer.launch({
+
+        /*const browser = await puppeteer.launch({
             headless:false
         });
         const page = await browser.newPage();
         await page.goto(req.body.url,{waitUntil : "networkidle2"});
         await page.screenshot({path: 'public/'+req.body.name, fullPage:true});
-        await browser.close();
+        await browser.close();*/
+        /*puppeteer.launch({
+            headless: false
+        }).then((browser) => {
+            //console.log(browser);
+            browser.newPage()
+                .then(page => page.goto(req.body.url, {waitUntil: "networkidle2"})
+                    .then(resp => page.screenshot({path: 'public/' + req.body.name, fullPage: true})
+                        .then(buffer => {
+                            browser.close();
+                            //convert.use(express.static('public'));
+                            res.header("Access-Control-Allow-Origin", "*");
+                            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                            res.send('http://localhost:3000/' + req.body.name)
+                        })))
+        }, (err) => {
+            console.error(err);
+            res.status(500);
+        });*/
+        console.log(req);
+        res.status(200).send(req);
 
-        convert.use(express.static('public'));
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.send('http://localhost:3000/'+req.body.name)
 
-    } catch (err) {
-        console.error(err);
-        res.status(500);
-    }
-});
+    });
 
-module.exports = router;
+
+}
+
+/* POST a url and name of file to get a png convert of it*/
+
