aa2857f17d79f5c98d07c1d991a4a83f309db7e1
Jakub Piotrowski <piotrowski.jakub72@gmail.com>
2018-04-17 13:25:19 +0200

    fix sending request

--- database/express-sequelize-test/app.js
+++ database/express-sequelize-test/app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+const cors= require('cors');
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-const puppeteer = require('puppeteer');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var comments = require('./routes/comments');
@@ -12,19 +12,20 @@ var screenshots = require('./routes/screenshots');
 const convertsRouter  = require('./routes/convert');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-//const bp=bodyParser.json();
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/component',express.static('component'));
+app.use('/download', express.static('public'));
 app.use('/', index);
 app.use('/users', users);
 app.use('/comments', comments);
--- database/express-sequelize-test/routes/convert.js
+++ database/express-sequelize-test/routes/convert.js
@@ -2,23 +2,36 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
 const convert = express();
-convert.use(cors());
+const path = require('path');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const model = require('../models/index');
 
+convert.use(cors());
+//convert.use(express.static('public/'));
 /* POST a url and name of file to get a png convert of it*/
-convert.post('/', async function (req, res){
+convert.post('/', async function (req, res) {
     try {
         const browser = await puppeteer.launch({
-            headless:false
+            headless: false
         });
         const page = await browser.newPage();
-        await page.goto(req.body.url,{waitUntil : "networkidle2"});
-        await page.screenshot({path: 'public/'+req.body.name, fullPage:true});
+        await page.goto(req.body.url, {waitUntil: "networkidle2"});
+        await page.screenshot({path: 'public/' + req.body.name, fullPage: true});
         await browser.close();
+        const title = Date.now();
+        const screenshot = await model.Screenshot.create({
+            title
+        });
 
-        convert.use(express.static('public'));
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.send('http://localhost:3000/'+req.body.name)
+        res.status(201).json({
+            error: false,
+            data: screenshot,
+            message: 'New Screenshot has been created.'
+
+        });
+        res.send(`${config.routes.backend.host}:${config.routes.backend.port}/download/${title}.png`)
+
 
     } catch (err) {
         console.error(err);
