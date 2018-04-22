08a8e3a133f71679520cd00786a8fe2b10431b3b
Jakub Piotrowski <piotrowski.jakub72@gmail.com>
2018-04-23 00:55:12 +0200

    fix chrome popup

--- database/express-sequelize-test/routes/convert.js
+++ database/express-sequelize-test/routes/convert.js
@@ -2,25 +2,22 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
 const convert = express();
-const path = require('path');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const model = require('../models/index');
 
 convert.use(cors());
-//convert.use(express.static('public/'));
+
 /* POST a url and name of file to get a png convert of it*/
 convert.post('/', async function (req, res) {
     try {
-        const browser = await puppeteer.launch({
-            headless: false
-        });
+        const browser = await puppeteer.launch();
         const title = Date.now();
         const page = await browser.newPage();
         await page.goto(req.body.url, {waitUntil: "networkidle2"});
         await page.screenshot({path: 'public/' + title + '.png', fullPage: true});
         await browser.close();
-        const screenshot = await model.Screenshot.create({
+        screenshot = await model.Screenshot.create({
             title
         });
 
