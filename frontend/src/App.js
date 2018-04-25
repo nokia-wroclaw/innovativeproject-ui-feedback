41f593e05e227bb494e21b81b34d58f38032ca50
Jakub Piotrowski <piotrowski.jakub72@gmail.com>
2018-04-25 11:09:43 +0200

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,20 +7,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World</h1>
-        </header>
-        <p className="App-intro">
-          Wcisnij przycisk, którego nie ma :)
-        </p>
-        <p><MuiThemeProvider><ListExample/></MuiThemeProvider></p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Feedback Control Panel</h1>
+                </header>
+                <p className="App-intro">
+
+                </p>
+                <p><MuiThemeProvider><ListExample/></MuiThemeProvider></p>
+            </div>
+        );
+    }
 }
 
 export default App;
